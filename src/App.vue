76df6a89ad30eabfabfaa6686ac1e0dc5c02110c
Eduardo Campos Gonçalves <edu.ggoncalves37@gmail.com>
2021-11-19 17:32:41 -0300

    feat: :sparkles: v-navigation-drawer created

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 
     <v-system-bar app>
       <v-row justify="center">
-        <strong>v-system-bar</strong>
+        <strong> v-system-bar </strong>
       </v-row>
     </v-system-bar>
   
@@ -12,9 +12,24 @@
 
     <v-app-bar app>
       <v-row justify="center">
-        <strong>v-app-bar</strong>
+        <strong> v-app-bar </strong>
       </v-row>
     </v-app-bar>
+
+    <!-- V-NAVIGATION-DRAWER BELOW  -->
+
+    <v-card height="81vh">
+      <v-navigation-drawer absolute permanent left>
+        <v-container fill-height>
+          <v-card-text>
+            <center>
+              <strong> v-navigation-drawer </strong>
+            </center>
+          </v-card-text>
+        </v-container>
+      </v-navigation-drawer>
+    </v-card>
+
     
   </v-app>
 
