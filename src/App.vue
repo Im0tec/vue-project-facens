2e54bee79d3d0f1d1f8a634274b785cd8e6bcace
Eduardo Campos Gonçalves <edu.ggoncalves37@gmail.com>
2021-11-19 17:21:43 -0300

    feat: :sparkles: v-app-bar created

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
-
-  <!-- V-SYSTEM-BAR BELOW  -->
-  
   <v-app>
+    <!-- V-SYSTEM-BAR BELOW  -->
+
     <v-system-bar app>
       <v-row justify="center">
         <strong>v-system-bar</strong>
       </v-row>
     </v-system-bar>
+  
+    <!-- V-APP-BAR BELOW  -->
+
+    <v-app-bar app>
+      <v-row justify="center">
+        <strong>v-app-bar</strong>
+      </v-row>
+    </v-app-bar>
+    
   </v-app>
+
 </template>
 
 <script>
